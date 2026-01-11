89d6a9744f10e52309994afd11ffe60207d96b34
Alina Zauer <sauer6699@gmail.com>
2026-01-12 01:12:03 +0500

    feat: Implement arguments parsing and --format option for CLI

--- src/cli.js
+++ src/cli.js
@@ -6,10 +6,7 @@ program
     .name('gendiff') 
     .description(`Compares two configuration files and shows a difference.`)
     .version('1.0.0') 
-
-// program
-//     .argument('<filepath1>', 'Path to first file')
-//     .argument('<filepath2>', 'Path to second file');
-// .arguments('<filepath1> <filepath2>')
+    .arguments('<filepath1> <filepath2>')
+    .option('-f, --format [type]', 'output format')
     
 export { program }
