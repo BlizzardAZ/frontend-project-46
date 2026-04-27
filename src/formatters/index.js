19887ad8f81b84469dfd9d25768dde9f5fdc3df4
Alina Zauer <sauer6699@gmail.com>
2026-04-28 00:47:36 +0500

    chore: run linter and fix style issues

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,18 +1,17 @@
-//Выбор форматеров реализуйте в файле formatters/index.js.
+// Выбор форматеров реализуйте в файле formatters/index.js.
 
-import formatStylish from "./stylishFormat.js"
-import formatPlain from "./plainFormat.js"
+import formatStylish from './stylishFormat.js'
+import formatPlain from './plainFormat.js'
 
 const getFormatter = (diffAST, formatName) => {
-    switch (formatName) {
-        case 'stylish':
-          return formatStylish(diffAST)
-        case 'plain':
-            return formatPlain(diffAST)
-        default:
-          throw new Error(`Unknown format: '${formatName}'!`)
-      }
-
+  switch (formatName) {
+    case 'stylish':
+      return formatStylish(diffAST)
+    case 'plain':
+      return formatPlain(diffAST)
+    default:
+      throw new Error(`Unknown format: '${formatName}'!`)
+  }
 }
 
 export default getFormatter
